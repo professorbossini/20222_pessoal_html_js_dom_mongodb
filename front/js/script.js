a2c5a43f536f26ec94d9421c4f493193b554c178
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-10 16:52:38 -0300

    cadastra filme com validacao de campos vazios

--- front/js/script.js
+++ front/js/script.js
@@ -14,3 +14,32 @@ async function obterFilmes() {
     celulaSinopse.innerHTML = filme.sinopse
   }
 }
+
+ async function cadastrarFilme(){
+  const URLCompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+  let tituloInput = document.querySelector('#tituloInput')
+  let sinopseInput = document.querySelector('#sinopseInput')
+  let titulo = tituloInput.value
+  let sinopse = sinopseInput.value
+  // somente adiciona se o usuário tiver digitado os dois valores
+  if (titulo && sinopse){
+    tituloInput.value = ""
+    sinopseInput.value = ""
+    await axios.post(URLCompleta, {titulo, sinopse})
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML = ""
+    obterFilmes()
+  }
+  //senão, exibe o alerta por até 2 segundos
+  else{
+    let alert = document.querySelector('.alert')
+    alert.classList.add('show')
+    alert.classList.remove('d-none')
+    setTimeout(() => {
+      alert.classList.remove('show')
+      alert.classList.add('d-none')
+    }, 2000)
+  }
+}
+ 
