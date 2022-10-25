c555f9f3899c6442638743eca0038bcbe70d53f5
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-25 19:37:04 -0300

    back end com cadastro e login com jwt

--- backend.js
+++ backend.js
@@ -1,18 +1,27 @@
-//pode tirar a sua string de conexão daqui agora
-const express = require ('express')
-const cors = require ('cors')
-const mongoose = require ('mongoose')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-const Filme = mongoose.model ("Filme", mongoose.Schema({
-  titulo: {type: String},
-  sinopse: {type: String}
+const Filme = mongoose.model("Filme", mongoose.Schema({
+  titulo: { type: String },
+  sinopse: { type: String }
 }))
 
+const usuarioSchema = mongoose.Schema({
+  login: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+})
+usuarioSchema.plugin(uniqueValidator)
+const Usuario = mongoose.model("Usuario", usuarioSchema)
+
 async function conectarAoMongoDB() {
-  await mongoose.connect(`mongodb+srv://professorbossini:professorbossini@cluster0.wttmkyk.mongodb.net/?retryWrites=true&w=majority`)
+  await mongoose.connect(`mongodb+srv://usuario:senha@cluster0.wttmkyk.mongodb.net/?retryWrites=true&w=majority`)
 }
 
 
@@ -27,16 +36,56 @@ app.post("/filmes", async (req, res) => {
   const sinopse = req.body.sinopse
   //monta um objeto agrupando os dados. Ele representa um novo filme
   //a seguir, construímos um objeto Filme a partir do modelo do mongoose
-  const filme = new Filme({titulo: titulo, sinopse: sinopse})
+  const filme = new Filme({ titulo: titulo, sinopse: sinopse })
   //save salva o novo filme na base gerenciada pelo MongoDB
   await filme.save()
   const filmes = await Filme.find()
   res.json(filmes)
 })
 
-  app.post('/login', (req, res) => {
-    
-  })
+app.post('/signup', async (req, res) => {
+  try {
+
+    const login = req.body.login
+    const password = req.body.password
+    const criptografada = await bcrypt.hash(password, 10)
+    const usuario = new Usuario({
+      login: login,
+      password: criptografada
+    })
+    const respMongo = await usuario.save()
+    console.log(respMongo)
+    res.status(201).end()
+  } catch (error) {
+    console.log(error)
+    res.status(409).end()
+  }
+})
+
+app.post('/login', async (req, res) => {
+  //login/senha que o usuário enviou
+  const login = req.body.login
+  const password = req.body.password
+  //tentamos encontrar no MongoDB
+  const u = await Usuario.findOne({login: req.body.login})
+  if(!u){
+    //senão foi encontrado, encerra por aqui com código 401
+    return res.status(401).json({mensagem: "login inválido"})
+  }
+  //se foi encontrado, comparamos a senha, após descriptográ-la
+  const senhaValida = await bcrypt.compare(password, u.password)
+  if (!senhaValida){
+    return res.status(401).json({mensagem: "senha inválida"})
+  }
+  //aqui vamos gerar o token e devolver para o cliente
+  const token = jwt.sign(
+      {login: login},
+      //depois vamos mudar para uma chave secreta de verdade 
+      "chave-secreta", 
+      {expiresIn: "1h"}
+  )
+  res.status(200).json({token: token})
+})
 
 
 //GET http://localhost:3000/hey
@@ -47,11 +96,11 @@ app.get('/hey', (req, res) => {
 
 
 app.listen(3000, () => {
-  try{
+  try {
     conectarAoMongoDB()
     console.log("up and running")
   }
-  catch (e){
+  catch (e) {
     console.log('Erro', e)
   }
 
